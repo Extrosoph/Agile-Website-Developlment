0d3f1f56697fa5185b052f1d4338f5de4e665ed8
theor <kennetha1>
2021-04-07 22:09:23 +0800

    fix some issues with init

--- webapp/webapp/__init__.py
+++ webapp/webapp/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from webapp import views, auth
 
 app = Flask(__name__)
 # db = SQLAlchemy(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
-if __name__ == "__main__":
-    # db.create_all()
-    app.run(debug=True)
+from webapp import views, auth
--- webapp/webapp/admin.py
+++ webapp/webapp/admin.py
@@ -1,4 +1,4 @@
-from webapp import app
+from webapp1 import app
 from flask import  redirect, url_for, render_template, request, session, flash
 
 
