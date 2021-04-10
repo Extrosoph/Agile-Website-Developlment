294b762fd681bd55892177f94f4e10290d77101e
theor <kennetha1>
2021-04-10 21:22:38 +0800

    removed model from init as it is not fully built

--- webapp/webapp/__init__.py
+++ webapp/webapp/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask, session
 from datetime import timedelta
 
 app = Flask(__name__)
+
+#Need to change secret key
 app.secret_key = "Extrosoph"
+
 # db = SQLAlchemy(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 #Set session variable to last only for 5 mintues instead of 30 days.
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-from webapp import views, auth, models
+from webapp import views, auth
