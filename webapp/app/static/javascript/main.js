34fb89318490d022835c2f7dc6e8fbcc1eded1ba
Extrosoph <theoridho@hotmail.com>
2021-04-20 21:59:32 +0800

    Changed to js to be universal for all html pages and will try out creating some graphs using python jinja and Chart.js later on.

---
+++ webapp/app/static/javascript/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    var href = document.location.href;
+    var lastPathSegment = href.substr(href.lastIndexOf('/') + 1);
+
+    //Check what page it is on
+    if(lastPathSegment == 'login' || lastPathSegment == 'signup') {
+
+        //Function to show and hide password
+        const password = $('#passwordInput') ;
+        $('#eye').click(function(){
+            if(password.prop('type') == 'password'){
+                $(this).addClass('fa-eye-slash');
+                password.attr('type','text')
+            }
+            else {
+                $(this).removeClass('fa-eye-slash');
+                password.attr('type','password');
+            }
+        })
+
+        //Check if forms are empty depending on the page
+        if(lastPathSegment == 'login') {
+            //Cool function to add later
+            $('#forget-password').click(function() {
+                $('h1').text("Forget Password");
+                $('#password').remove();
+                $('#login').remove();
+                $('#forget-password').remove();
+                $('#signup').remove();
+                $('#myform').append('<button type="submit" id="Verify" class="button">Verify</button>')
+            })
+
+            $('#login').click(function() {
+                if ($('#email').val().length === 0 || $('#passwordInput').val().length === 0) {
+                    alert('Need to fill all fields!');
+                    return false;
+                }
+            })
+        }
+
+        else {
+            $('#signup').click(function() {
+                if ($('#email').val().length === 0 || $('#passwordInput').val().length === 0 || $('#username').val().length === 0) {
+                    alert('Need to fill all fields!');
+                    return false;
+                }
+            })
+        }
+    }
+
+})
