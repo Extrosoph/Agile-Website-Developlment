d6ae91b938a5ae8405c9928b7683c6d381816354
Saksham Chopra <22723671@student.uwa.edu.au>
2021-05-16 16:51:28 +0800

    assessment html

--- migrations/versions/91c86b48ff44_.py
+++ migrations/versions/91c86b48ff44_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '91c86b48ff44'
-down_revision = 'eeb259f3e82e'
+down_revision = None
 branch_labels = None
 depends_on = None
 
